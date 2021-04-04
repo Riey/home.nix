07034189ff49ff9cffce6046ebd147ee9888ba3e
Riey <creeper844@gmail.com>
2021-04-04 19:47:56 +0900

    Move set gpg_tty to before source p10k

--- zsh.nix
+++ zsh.nix
@@ -15,6 +15,7 @@
       }
     ];
     initExtraBeforeCompInit = ''
+    export GPG_TTY=$(tty)
     # Enable Powerlevel10k instant prompt. Should stay close to the       top of ~/.zshrc.
     # Initialization code that may require console input (password p      rompts, [y/n]
     # confirmations, etc.) must go above this block; everything else       may go below.
@@ -27,9 +28,6 @@
     '';
     # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
     initExtra = "[[ ! -f ~/.config/nixpkgs/p10k.zsh ]] || source ~/.config/nixpkgs/p10k.zsh";
-    sessionVariables = {
-      GPG_TTY = "$(tty)";
-    };
     shellGlobalAliases = {
       cat = "bat";
       ls = "lsd";
